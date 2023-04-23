6146a4804927e9fa7b9cfceaa002ef9939a0e295
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2023-04-23 12:18:04 +0200

    Initial commit

--- rest/dao/StudentDAO.class.php
+++ rest/dao/StudentDAO.class.php
@@ -31,23 +31,34 @@ class StudentDao{
         $stmt = $this->conn->prepare("SELECT * FROM students WHERE id = :id");
         $stmt->execute(['id' => $id]);
         //return $stmt->fetchAll(PDO::FETCH_ASSOC); //returns you an array with one element, to access it, you would have to go student0.id
-        return $stmt->fetch(); //here, returns a single item, access it just by student.respone, a ne mora student0.response jer je jedan objekat
+        //return $stmt->fetch(); //here, returns a single item, access it just by student.respone, a ne mora student0.response jer je jedan objekat
+        return $stmt->fetchAll();
 
     }
 
     //Method used to add  students from database
-    public function add($first_name, $last_name){
-        $stmt = $this->conn->prepare("INSERT INTO students (first_name, last_name) VALUES(?, ?)");
-       $result = $stmt->execute([$first_name, $last_name]);
-       return $result;
-       //ovdje je redolsijed bitan jer prvi ? mijenja ime, a drugi prezime
+    public function add($student){
+       $stmt = $this->conn->prepare("INSERT INTO students (first_name, last_name) VALUES(:first_name, :last_name)");
+       $stmt->execute($student);
+       $student['id'] = $this->conn->lastInsertId(); //ovo nam vraca id of the last student
+       return $student;                              //ovo vracamo da vidimo koga smo dodali u DB
+      //da smo pored imena i prezimena, htjeli dodati jos value za statu, onda bismo to samo tako gore dodali, ali za parametar jer pise student ne treba nista vise
+      //tipe sto nam je parametar student postizemo da ne trebamo pisati tacan broj parametara koji se trebaju promijeniti i pozivati ih u funkciju, vec imamo samo student kao
+      //array vrijednosti i onda u ostaku koda samo napisemo nazive tih varijabli na koje zelimo utjecati
+      //ovdje je redolsijed bitan jer prvi ? mijenja ime, a drugi prezime
     }
 
       //Method used to update students from database
-      public function update($first_name, $last_name, $id){
+      public function update($student, $id){
+        $student['id'] = $id; 
+        //kod add, ovo nismo morali raditi jer nismo imali ovaj treci, tj. drugi arg jer sad first i last name gledam kao jedan arg kroz $student, 
+        //ali bez ovoga bi nam u ovoj metodi pokazalo error
+        //mi kad napisemo $student, onda se odnosimo sa sve atribute studente, koje smo definisali u narednom koraku, ako hocemo da idemo samo
+        // $stmt->execute($student);, onda id iz parametra mora postati dio ovoga $student i to je uradeno u prvoj liniji koda ove funkcije
         $stmt = $this->conn->prepare("UPDATE students SET first_name = :first_name, last_name = :last_name WHERE id = :id");
-        $stmt->execute(['first_name' => $first_name, 'last_name' => $last_name, 'id' => $id]);
-        //ovdje u execute npr redoslijed nije bitan, jer su vezani preko naming, a ne position
+        $stmt->execute($student);
+        return $student;
+       //ovaj return ovdje je ono na sto ce string data upucivati iz index.php, znaci neka vrijednost ce biti spasena u data
     }
 
     
